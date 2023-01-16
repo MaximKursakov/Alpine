aa016b55921b580f48e89d317250bd29cb96dc35
MaximKursakov <maximcarlos@gmx.de>
2023-01-16 11:20:11 +0100

    header styling

--- alpine.js
+++ alpine.js
@@ -67,6 +67,34 @@ document.addEventListener('alpine:init', () => {
             description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Officia dolores dicta laboriosam tempora! Quam ducimus quaerat veritatis vitae, eius asperiores pariatur a quidem quibusdam odit molestiae nostrum repudiandae quasi ea!",
             price: 60.99,
             unit: 200,
+        },
+        {
+            title: "Vitamin B Creme",
+            img: `Product1.png`,
+            description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Officia dolores dicta laboriosam tempora! Quam ducimus quaerat veritatis vitae, eius asperiores pariatur a quidem quibusdam odit molestiae nostrum repudiandae quasi ea!",
+            price: 14.95,
+            unit: 200,
+        },
+        {
+            title: "Creme Paradaoxe",
+            img: `Product2.png`,
+            description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Officia dolores dicta laboriosam tempora! Quam ducimus quaerat veritatis vitae, eius asperiores pariatur a quidem quibusdam odit molestiae nostrum repudiandae quasi ea!",
+            price: 60.99,
+            unit: 300,
+        },
+        {
+            title: "Line Combat Fluid",
+            img: `Product3.png`,
+            description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Officia dolores dicta laboriosam tempora! Quam ducimus quaerat veritatis vitae, eius asperiores pariatur a quidem quibusdam odit molestiae nostrum repudiandae quasi ea!",
+            price: 35.99,
+            unit: 200,
+        },
+        {
+            title: "Gel exfoliant du corps",
+            img: `Product4.png`,
+            description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Officia dolores dicta laboriosam tempora! Quam ducimus quaerat veritatis vitae, eius asperiores pariatur a quidem quibusdam odit molestiae nostrum repudiandae quasi ea!",
+            price: 45.99,
+            unit: 400,
         },]
     })
 })
