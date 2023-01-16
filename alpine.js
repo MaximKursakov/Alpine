804de0ade441676dd66fbeca500b3eb0c6c70aff
MaximKursakov <maximcarlos@gmx.de>
2023-01-16 10:06:05 +0100

    price per unit calculated

--- alpine.js
+++ alpine.js
@@ -7,6 +7,9 @@ document.addEventListener('alpine:init', () => {
             this.wishlist.push(item);
         }
         },
+        pricePerUnit(item) {
+            return ((item.price / item.unit) * 100).toFixed(2)
+        },
         basketTotal: 0,
         basketDuplicates: [],
         duplicateAmount: [],
@@ -34,31 +37,36 @@ document.addEventListener('alpine:init', () => {
             title: "Vitamin B Creme",
             img: `Product1.png`,
             description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Officia dolores dicta laboriosam tempora! Quam ducimus quaerat veritatis vitae, eius asperiores pariatur a quidem quibusdam odit molestiae nostrum repudiandae quasi ea!",
-            price: 30.99,
+            price: 14.95,
+            unit: 200,
         },
         {
             title: "Creme Paradaoxe",
             img: `Product2.png`,
             description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Officia dolores dicta laboriosam tempora! Quam ducimus quaerat veritatis vitae, eius asperiores pariatur a quidem quibusdam odit molestiae nostrum repudiandae quasi ea!",
             price: 60.99,
+            unit: 300,
         },
         {
             title: "Line Combat Fluid",
             img: `Product3.png`,
             description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Officia dolores dicta laboriosam tempora! Quam ducimus quaerat veritatis vitae, eius asperiores pariatur a quidem quibusdam odit molestiae nostrum repudiandae quasi ea!",
             price: 35.99,
+            unit: 200,
         },
         {
             title: "Gel exfoliant du corps",
             img: `Product4.png`,
             description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Officia dolores dicta laboriosam tempora! Quam ducimus quaerat veritatis vitae, eius asperiores pariatur a quidem quibusdam odit molestiae nostrum repudiandae quasi ea!",
             price: 45.99,
+            unit: 400,
         },
         {
             title: "Enriched Moisturizing Body Lotion",
             img: `Product5.png`,
             description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Officia dolores dicta laboriosam tempora! Quam ducimus quaerat veritatis vitae, eius asperiores pariatur a quidem quibusdam odit molestiae nostrum repudiandae quasi ea!",
             price: 60.99,
+            unit: 200,
         },]
     })
 })
